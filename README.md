3ac25188bf05a5c19a51a694a26746bfda2f16ea
mmlevy <gx200df@aol.com>
2020-02-02 19:40:47 -0800

    Use express-fileupload middleware to be able to receive the uploaded file. Store file in uploads folder.

---
+++ README.md
@@ -0,0 +1,7 @@
+TODO: Write a more detailed README and verify these steps work on fresh environment
+
+Pre-requisite: Nodejs v10 is required to be installed on the machine in order to run this. Might work on other versions, but I haven't tested that
+
+1. Run "npm install".
+2. Run "grunt build" to compile the application. The compiled application will end up in a directory called "dist".
+3. Finally, run "grunt run" to launch the application. The app is available on localhost:3000
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as fileUpload from 'express-fileupload';
 import * as path from 'path';
 import * as http from 'http';
 
@@ -6,12 +7,28 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '')));
+app.use(fileUpload({
+    createParentPath: true
+}));
 
-// TODO - Implement api for file conversion
 app.post('/api/upload', (req, res) => {
-    res.send({
-       test: "test success"
+    console.log(req.files.file);
+    if (!req.files) {
+        return res.status(400).send({ error: 'No file was received' });
+    }
+    let file = req.files.file;
+    file.mv('./uploads/' + file.name, (err) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
     });
+    return res.status(200).send('File uploaded!');
+});
+
+app.post('/api/convert', (req, res) => {
+    console.log(req.fileName);
+    // TODO: Convert file and return it
+    return res.status(200).send('File converted');
 });
 
 // Catch all other routes and return to index file
